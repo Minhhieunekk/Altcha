c1a9baf06383f7e2be765279114b8674d3344608
hieu <iwillwin2211@gmail.com>
2025-03-26 14:45:41 +0700

    ALtcha commit

---
+++ src/main/java/com/example/altcha/AltchaApplication.java
@@ -0,0 +1,13 @@
+package com.example.altcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AltchaApplication.class, args);
+    }
+
+}
